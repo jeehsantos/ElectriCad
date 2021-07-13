a705b263562497bb7a600b57bb9b1a77b1a541f2
Jennipher Oropesa <je.oropesaf@gmail.com>
2021-07-13 12:54:33 +1200

    The experience section have been changed
    Changes were made in html and css. It has included javascript to give carousel funcionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -202,7 +202,51 @@
     items: 1
   });
 
-  
+ // Clients carousel (uses the Owl Carousel library)
+ $(".clients-carousel").owlCarousel({
+  autoplay: true,
+  dots: true,
+  loop: true,
+  responsive: {
+    0: {
+      items: 2
+    },
+    768: {
+      items: 4
+    },
+    900: {
+      items: 6
+    }
+  }
+});
+
+// jQuery counterUp
+$('[data-toggle="counter-up"]').counterUp({
+  delay: 10,
+  time: 1000
+});
+
+// Testimonials carousel (uses the Owl Carousel library)
+$(".testimonials-carousel").owlCarousel({
+  autoplay: true,
+  dots: true,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    768: {
+      items: 1
+    },
+    900: {
+      items: 2
+    },
+    1400: {
+      items: 3
+    }
+  }
+});
+   
   // Init AOS
   function aos_init() {
     AOS.init({
